e37bae260793d25795d90714fb498ac34614a767
Darren Robertson <dazakip@gmail.com>
2017-01-23 17:47:29 +0000

    added package of examples how to use SQLite on the .db file

---
+++ src/sqlite_examples/CREATE_db_and_table.java
@@ -0,0 +1,49 @@
+package sqlite_examples; /**
+ * Created by Darren on 23/01/2017.
+ */
+import java.sql.*;
+
+public class CREATE_db_and_table
+{
+    public static void main( String args[] )
+    {
+        /* ************************************************************************** CREATE A DATABASE
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        **********************************************************************/
+
+        //***************************************************************************** CREATE TABLE
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+        //*/
+
+    }
+}
