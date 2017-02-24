e7c87f4c60f677339951c3a73a10bef16c475f31
Darren Robertson <dazakip@gmail.com>
2017-02-24 01:14:05 +0000

    a lot has changed from last commit  - see logbook.txt

--- src/example/ExController.java
+++ src/example/ExController.java
@@ -1,9 +1,20 @@
 package example;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView ;
 import java.awt.*;
+import java.io.FileNotFoundException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class ExController {
     @FXML
@@ -14,6 +25,15 @@ public class ExController {
     private ImageView LEFTHAZ;
     @FXML
     private ImageView RIGHTHAZ;
+    @FXML
+    private ImageView MAST;
+    @FXML
+    private ImageView MAHLI;
+    @FXML
+    private javafx.scene.control.Label numSol;
+
+    private int sol = 0;
+
 
 
     // Reference to the main application.
@@ -24,6 +44,7 @@ public class ExController {
      * The constructor is called before the initialize() method.
      */
     public ExController() {
+        System.out.println("oioi");
     }
 
     /**
@@ -32,10 +53,40 @@ public class ExController {
      */
     @FXML
     private void initialize() {
-        // Initialize the person table with the two columns.
-        Image g = new Image("file:C:\\Users\\Dazak\\Desktop\\ssssssssasdsadasdasd.png");
-        FRONTHAZ.setImage(g);
-        LEFTHAZ.setImage(g);
+        //for (int sol = 0; sol <=100;sol++) {
+        int sol = 0;
+
+       // }
+
+        ScheduledExecutorService execService = Executors.newScheduledThreadPool(5);
+
+        execService.scheduleAtFixedRate(()->{
+            //The repetitive task
+            loop();
+
+        }, 0, 650L, TimeUnit.MILLISECONDS);
+
+
+
+
+    }
+
+    private void loop() {
+
+        FRONTHAZ.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\FHAZ_LEFT_A\\" + sol + ".jpg"));
+        REARHAZ.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\RHAZ_LEFT_A\\" + sol + ".jpg"));
+        LEFTHAZ.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\NAV_LEFT_A\\" + sol + ".jpg"));
+        RIGHTHAZ.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\NAV_RIGHT_A\\" + sol + ".jpg"));
+        MAHLI.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\MAHLI\\" + sol + ".jpg"));
+        MAST.setImage(new Image("file:C:\\Users\\Dazak\\Desktop\\mars_imgs\\MAST_LEFT\\" + sol + ".jpg"));
+
+        Platform.runLater(
+                () -> {
+                    numSol.setText("Sol: "+ Integer.toString(sol));
+                }
+        );
+            sol++;
+
     }
 
     /**
