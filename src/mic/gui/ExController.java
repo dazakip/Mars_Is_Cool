d3d4427953d29c5d0aa2ac8e0249db6672e59efc
Darren Robertson <dazakip@gmail.com>
2017-03-15 18:18:42 +0000

    see logbook

--- src/mic/gui/ExController.java
+++ src/mic/gui/ExController.java
@@ -9,6 +9,9 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -29,9 +32,9 @@ public class ExController {
                         MAST_th, MAHLI_th, MARDI_th, CHEM_th, CURATED_th;
     private ArrayList<ImageView> cameras;
     @FXML
-    private Label solLabel;
-    @FXML
     private TextField solTextField;
+    @FXML
+    private Label mainView;
     @FXML
     private WebView map;
     private WebEngine webEngine;
@@ -39,13 +42,18 @@ public class ExController {
     private Slider solSlider;
     @FXML
     private Button playpause, forwardStep, backwardStep, addMarker;
+    @FXML
+    private LineChart lineChart;
+    @FXML
+    private NumberAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
 
-    private String imagePath;
+    // fields critical to control flow
+    private ImageLocations imgLocs;
     private int currentSol;
     private ScheduledExecutorService execService;
     private Future<?> future;
-
-    private String selectedViewPath;
 
     // Reference to the main application.
     private MainApp mainApp;
@@ -56,9 +64,8 @@ public class ExController {
     public ExController() {
         currentSol = -1;
         execService = Executors.newScheduledThreadPool(1);
-        cameras = new ArrayList<ImageView>();
-        imagePath = "C:\\Users\\Dazak\\Desktop\\mic_imgs\\";
-        selectedViewPath = imagePath+"LEFTHAZ\\";
+        cameras = new ArrayList<>();
+        imgLocs = new ImageLocations();
     }
 
     /**
@@ -67,103 +74,15 @@ public class ExController {
      */
     @FXML
     private void initialize() {
-
         // grab mars map and laod into WebView
         URL url = getClass().getResource("map/openlayers_mic.html");
-
         map.getEngine().load(url.toExternalForm());
         webEngine = map.getEngine();
         webEngine.load(url.toExternalForm());
 
-
-        // add cameras to arraylist
-        cameras.add(FRONTHAZ_th);
-        cameras.add(REARHAZ_th);
-        cameras.add(LEFTHAZ_th);
-        cameras.add(RIGHTHAZ_th);
-        cameras.add(MAST_th);
-        cameras.add(MAHLI_th);
-        cameras.add(MARDI_th);
-        cameras.add(CHEM_th);
-
-        playScheduler();
-
-        for (ImageView cam:cameras){
-            cam.setOnMouseClicked(event -> {
-                selectedViewPath = imagePath + cam.getId().substring(0, cam.getId().length() - 3)+"\\";
-                updateCamera();
-            });
-        }
-
-        //slider listener
-        solSlider.valueProperty().addListener((arg0, arg1, arg2) -> {
-
-            solTextField.setText(Integer.toString((int) solSlider.getValue()));
-            currentSol = (int) solSlider.getValue();
-            updateCamera();
-
-
-        });
-
-        solTextField.focusedProperty().addListener((arg0, oldPropertyValue, newPropertyValue) -> {
-            if (newPropertyValue)
-            {
-                pauseScheduler();
-            }
-            else
-            {
-                currentSol = Integer.parseInt(solTextField.getText());
-                playScheduler();
-            }
-        });
-
-        addMarker.setOnAction(event -> {
-            runLater(() -> {
-                webEngine.executeScript("panToCuriosity();");
-            });
-        });
-
-        playpause.setOnAction(event -> {
-            if (playpause.getText().equals("Play")) {
-                playScheduler();
-            }
-            else {
-                pauseScheduler();
-            }
-        });
-
-        forwardStep.setOnAction(event -> {
-            pauseScheduler();
-            loop();
-        });
-
-        backwardStep.setOnAction(event -> {
-            pauseScheduler();
-            currentSol = currentSol-2;
-            loop();
-        });
-    }
-
-    /**
-     * Used whenever program is paused is to be resumed
-     */
-    private void playScheduler() {
-        future = execService.scheduleAtFixedRate(()->{
-            //The repetitive task
-            if (currentSol<=1500)
-                loop();
-        }, 3000L, 1000L, TimeUnit.MILLISECONDS);
-        playpause.setText("Pause");
-    }
-
-    /**
-     * Used whenever program is to be paused
-     * Even though is single line makes it easier to understand
-     * code/more workable if any changes in future
-     */
-    private void pauseScheduler() {
-        future.cancel(true);
-        playpause.setText("Play");
+        playScheduler(false);
+        establishListeners();
+        createLineChart();
     }
 
     /**
@@ -178,8 +97,133 @@ public class ExController {
             solSlider.setValue(currentSol);
             webEngine.executeScript("highlightPath("+currentSol+");");
         });
-        //webEngine.executeScript("highlightSolLine();");
-        //addMarker.fire();
+    }
+
+    /**
+     * Method contain all the various listeners used in the application, called from initilize()
+     * grouped together to easily find/hide away
+     */
+    private void establishListeners() {
+        //sol slider listener
+        solSlider.valueProperty().addListener((arg0, arg1, arg2) -> {
+            solTextField.setText(Integer.toString((int) solSlider.getValue()));
+            currentSol = (int) solSlider.getValue();
+            updateCamera();
+        });
+
+        // text field showing the current sol listener
+        solTextField.focusedProperty().addListener((arg0, oldPropertyValue, newPropertyValue) -> {
+            if (newPropertyValue){
+                pauseScheduler();
+            } else {
+                currentSol = Integer.parseInt(solTextField.getText());
+                playScheduler(true);
+            }
+        });
+
+        // button that play/pauses the system
+        playpause.setOnAction(event -> {
+            if (playpause.getText().equals("Play")) {
+                playScheduler(true);
+            } else {
+                pauseScheduler();
+            }
+        });
+
+        // button that move system 1 sol forward
+        forwardStep.setOnAction(event -> {
+            pauseScheduler();
+            loop();
+        });
+
+        // button that move system 1 sol back
+        backwardStep.setOnAction(event -> {
+            pauseScheduler();
+            currentSol = currentSol-2;
+            loop();
+        });
+
+        // add cameras to arraylist
+        cameras.add(FRONTHAZ_th);
+        cameras.add(REARHAZ_th);
+        cameras.add(LEFTHAZ_th);
+        cameras.add(RIGHTHAZ_th);
+        cameras.add(MAST_th);
+        cameras.add(MAHLI_th);
+        cameras.add(MARDI_th);
+        cameras.add(CHEM_th);
+        cameras.add(CURATED_th);
+
+        // add listener to all cameras that updates the main view with the thumbnail view clicked
+        for (ImageView cam:cameras){
+            cam.setOnMouseClicked(event -> {
+                String camalam = cam.getId().substring(0, cam.getId().length() - 3);
+                imgLocs.setSelectedView(camalam);
+                mainView.setText(imgLocs.getCamLabel(camalam));
+                updateCamera();
+            });
+        }
+
+        /**************************************************************************************************************
+         * Button used for various tests ---------------- DELETE AT THE END
+         */
+        addMarker.setOnAction(event ->
+                runLater(() -> webEngine.executeScript("panToCuriosity();")));
+        /**************************************************************************************************************/
+    }
+
+    private void createLineChart(){
+        lineChart.setTitle("Stock Monitoring, 2010");
+        xAxis.setLabel("TIMEEEEEEEEEE");
+        yAxis.setLabel("DATA REEEEEEE");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        xAxis.setLabel("DATA WEEEEWWWWW");
+        yAxis.setLabel("TIMEEEE");
+
+        //populating the series with data
+
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        series.getData().add(new XYChart.Data(6, 36));
+        series.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+        lineChart.getData().add(series);
+    }
+    /**
+     * Used whenever program is paused is to be resumed
+     */
+    private void playScheduler(boolean immediately) {
+        long delay = 3000L;
+        // for restarting scheduler quickly when changing sol/pressing play
+        if (immediately)
+            delay = 500L;
+
+        future = execService.scheduleAtFixedRate(()->{
+            //The repetitive task
+            if (currentSol<=1500)
+                loop();
+        }, delay, 1000L, TimeUnit.MILLISECONDS);
+        playpause.setText("Pause");
+
+    }
+
+    /**
+     * Used whenever program is to be paused
+     * Even though is single line makes it easier to understand
+     * code/more workable if any changes in future
+     */
+    private void pauseScheduler() {
+        future.cancel(true);
+        playpause.setText("Play");
     }
 
     /**
@@ -187,20 +231,19 @@ public class ExController {
      */
     private void updateCamera() {
 
-
-        if (new File(selectedViewPath + currentSol + ".jpg").isFile()) {
+        if (new File(imgLocs.getSelectedView() + currentSol + ".jpg").isFile()) {
             MAINVIEW.setOpacity(1);
-            MAINVIEW.setImage(new Image("file:" + selectedViewPath + currentSol + ".jpg"));
+            MAINVIEW.setImage(new Image("file:" + imgLocs.getSelectedView() + currentSol + ".jpg"));
         }
         else {
             MAINVIEW.setOpacity(0.75);
         }
 
-        // for each cam view update view
+        // for each thumbnail view update
         for (ImageView currCam : cameras) {
-            if (new File(imagePath + currCam.getId() + "\\" + currentSol + ".jpg").isFile()) {
+            if (new File(imgLocs.getCamera(currCam.getId()) + "\\" + currentSol + ".jpg").isFile()) {
                 currCam.setOpacity(1);
-                currCam.setImage(new Image("file:"+imagePath+currCam.getId()+"\\" + currentSol + ".jpg"));
+                currCam.setImage(new Image("file:"+imgLocs.getCamera(currCam.getId())+"\\" + currentSol + ".jpg"));
             }
             else {currCam.setOpacity(0.75);}
         }
@@ -208,8 +251,6 @@ public class ExController {
 
     /**
      * Is called by the main application to give a reference back to itself.
-     *
-     * @param mainApp
      */
     public void setMainApp(MainApp mainApp) {this.mainApp = mainApp;}
 }
---
+++ src/mic/gui/ImageLocations.java
@@ -0,0 +1,108 @@
+package mic.gui;
+
+/**
+ * Created by Dazak on 15/03/2017.
+ */
+public class ImageLocations {
+
+    private String folderPath;
+    private String selectedViewPath;
+
+    private String lastFRONTHAZ;
+    private String lastREARHAZ;
+    private String lastLEFTHAZ;
+    private String lastRIGHTHAZ;
+    private String lastMAST;
+    private String lastMARDI;
+    private String lastMAHLI;
+    private String lastCHEM;
+
+    public ImageLocations () {
+        folderPath = "C:\\Users\\Dazak\\Desktop\\mic_imgs\\";
+        selectedViewPath = folderPath+"LEFTHAZ\\";
+
+        lastFRONTHAZ = folderPath +"FRONTHAZ\\0";
+        lastREARHAZ = folderPath +"REARHAZ\\0";
+        lastRIGHTHAZ = folderPath +"RIGHTHAZ\\0";
+        lastLEFTHAZ = folderPath +"LEFTHAZ\\0";
+        lastMAHLI = folderPath +"MAHLI\\0";
+        lastMAST = folderPath +"MAST\\0";
+        lastMARDI = folderPath +"MARDI\\0";
+        lastCHEM = folderPath +"CHEM\\0";
+    }
+
+    public String getCamera(String camera) {
+        return folderPath+camera+"\\";
+    }
+
+    public String getSelectedView(){
+        return selectedViewPath;
+    }
+
+    public void setSelectedView(String newCamera) {
+        selectedViewPath = folderPath+newCamera+"\\";
+    }
+
+    public void setLastAvailable(String camera, int sol) {
+        if (camera.equals("FRONTHAZ"))
+            lastFRONTHAZ = folderPath+camera+"\\"+sol;
+        else if (camera.equals("REARHAZ"))
+            lastREARHAZ = folderPath+camera+"\\"+sol;
+        else if (camera.equals("LEFTHAZ"))
+            lastLEFTHAZ = folderPath+camera+"\\"+sol;
+        else if (camera.equals("RIGHTHAZ"))
+            lastRIGHTHAZ = folderPath+camera+"\\"+sol;
+        else if (camera.equals("MAHLI"))
+            lastMAHLI = folderPath+camera+"\\"+sol;
+        else if (camera.equals("MAST"))
+            lastMAST = folderPath+camera+"\\"+sol;
+        else if (camera.equals("MARDI"))
+            lastMARDI = folderPath+camera+"\\"+sol;
+        else if (camera.equals("CHEM"))
+            lastCHEM = folderPath+camera+"\\"+sol;
+    }
+
+    public String getLastAvailable(String camera) {
+        if (camera.equals("FRONTHAZ"))
+            return lastFRONTHAZ;
+        else if (camera.equals("REARHAZ"))
+            return lastREARHAZ;
+        else if (camera.equals("LEFTHAZ"))
+            return lastLEFTHAZ;
+        else if (camera.equals("RIGHTHAZ"))
+            return lastRIGHTHAZ;
+        else if (camera.equals("MAHLI"))
+            return lastMAHLI;
+        else if (camera.equals("MAST"))
+            return lastMAST;
+        else if (camera.equals("MARDI"))
+            return lastMARDI;
+        else if (camera.equals("CHEM"))
+            return lastCHEM;
+
+        return null;
+    }
+
+    public String getCamLabel(String camera) {
+        if (camera.equals("FRONTHAZ"))
+            return "Front Hazard Camera";
+        else if (camera.equals("REARHAZ"))
+            return "Rear Hazard Camera";
+        else if (camera.equals("LEFTHAZ"))
+            return "Left Hazard Camera";
+        else if (camera.equals("RIGHTHAZ"))
+            return "Right Hazard Camera";
+        else if (camera.equals("MAHLI"))
+            return "Mars Hand Lens Imager";
+        else if (camera.equals("MAST"))
+            return "Mast Camera";
+        else if (camera.equals("MARDI"))
+            return "Mars Descent Imager";
+        else if (camera.equals("CHEM"))
+            return "Chemistry Camera";
+        else if (camera.equals("CURATED"))
+            return "Curated View";
+
+        return null;
+    }
+}
